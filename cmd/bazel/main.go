a16b92d2318a01cdc441f00b2121af40d4b0bed7
Ryan Michela <rmichela@salesforce.com>
2019-04-16 22:04:17 -0400

    Basic bazel execution

--- cmd/bazel/main.go
+++ cmd/bazel/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// Capture the location of the Java 8 JDK and set JAVA_HOME
+	jdk, err := exec.Command("/usr/libexec/java_home", "--version", "1.8").Output()
+	if err != nil {
+		panic(err)
+	}
+	os.Setenv("JAVA_HOME", strings.TrimSpace(string(jdk)))
+
+	// Execute the selected real bazel entry point
+	binary := "/usr/local/Cellar/bazel/0.23.2/libexec/bin/bazel"
+	args := os.Args
+	env := os.Environ()
+
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
 }
